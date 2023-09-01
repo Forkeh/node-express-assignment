2a21a90298270911f56119c44f1e9edf565a4256
Forkeh <forkeh@gmail.com>
2023-09-01 18:29:48 +0200

    POST request is working

--- dist/backend/server.js
+++ dist/backend/server.js
@@ -15,9 +15,18 @@ app.get("/data", async (_req, res) => {
         res.json(users);
     }
     catch (error) {
-        console.log("Couldn't get file");
-        console.log(error);
+        console.log("Error reading data file:", error);
+        res.status(500).send("Internal Server Error");
     }
+});
+app.post("/data", async (req, res) => {
+    const newArtist = req.body;
+    newArtist.id = new Date().getTime();
+    const data = await fs.readFile("./dist/data/data.json", "utf8");
+    const artists = JSON.parse(data);
+    artists.push(newArtist);
+    fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
+    res.json(artists);
 });
 app.listen(port, () => {
     console.log(`Server is running on ${port}!`);
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -15,18 +15,32 @@ app.get("/", (_req, res) => {
     res.send("Root route! 🔥");
 });
 
-//----- Get data ----- //
+//----- GET data ----- //
 app.get("/data", async (_req, res) => {
     try {
-        const data = await fs.readFile("./dist/data/data.json", "utf8");
+        const data: string = await fs.readFile("./dist/data/data.json", "utf8");
 
-        const users = JSON.parse(data);
+        const users: object[] = JSON.parse(data);
 
         res.json(users);
     } catch (error) {
-        console.log("Couldn't get file");
-        console.log(error);
+        console.log("Error reading data file:", error);
+        res.status(500).send("Internal Server Error");
     }
+});
+
+//----- POST data ----- //
+app.post("/data", async (req, res) => {
+    const newArtist = req.body;
+    newArtist.id = new Date().getTime();
+
+    const data: string = await fs.readFile("./dist/data/data.json", "utf8");
+
+    const artists: object[] = JSON.parse(data);
+    artists.push(newArtist);
+
+    fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
+    res.json(artists);
 });
 
 //----- Start server on chosen port ----- //
