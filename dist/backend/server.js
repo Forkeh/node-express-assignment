dc8d60e9875b24a81a13d77824e6238c9be9e86a
Forkeh <forkeh@gmail.com>
2023-09-01 18:13:44 +0200

    Get request works

--- dist/backend/server.js
+++ dist/backend/server.js
@@ -1,9 +1,24 @@
+import fs from "fs/promises";
 import express from "express";
 import cors from "cors";
 const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
+app.get("/", (_req, res) => {
+    res.send("Root route! 🔥");
+});
+app.get("/data", async (_req, res) => {
+    try {
+        const data = await fs.readFile("./dist/data/data.json", "utf8");
+        const users = JSON.parse(data);
+        res.json(users);
+    }
+    catch (error) {
+        console.log("Couldn't get file");
+        console.log(error);
+    }
+});
 app.listen(port, () => {
-    console.log(`Server is running on ${port}!!!!`);
+    console.log(`Server is running on ${port}!`);
 });
