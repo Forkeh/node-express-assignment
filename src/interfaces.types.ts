91576d37780fd6bf2fa791ef928ad522ec57c641
Forkeh <forkeh@gmail.com>
2023-09-03 12:01:35 +0200

    created interface for artist objects

---
+++ src/interfaces.types.ts
@@ -0,0 +1,11 @@
+interface Artist {
+    name: string;
+    birthdate: string;
+    activeSince: number;
+    genres: string[];
+    labels: string;
+    website: string;
+    image: string;
+    shortDescription: string;
+    id?: number;
+}
