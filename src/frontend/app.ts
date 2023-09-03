e09b4a86404bc7dbd37b8b0e76b3dbd812a91490
Forkeh <forkeh@gmail.com>
2023-09-03 18:53:57 +0200

    artists get shown on page load (when server is running)

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -1,1 +1,11 @@
-console.log('Hello there!');
+import { getArtists, artists } from "./helpers/restAPI.js";
+import { showArtists } from "./artists/showArtists.js";
+
+window.addEventListener("load", app);
+
+async function app() {
+    const artists: Artist[] = await getArtists();
+    // console.table(artists);
+
+    showArtists(artists)
+}
