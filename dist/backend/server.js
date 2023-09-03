a33f16a8179fd019e953e193600c976fc8b1242f
Forkeh <forkeh@gmail.com>
2023-09-03 12:01:00 +0200

    added POST and PUT requests

--- dist/backend/server.js
+++ dist/backend/server.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 3000;
 app.use(express.json());
 app.use(cors());
-app.get("/", (_req, res) => {
+app.get("/", (req, res) => {
     res.send("Root route! 🔥");
 });
-app.get("/data", async (_req, res) => {
+app.get("/data", async (req, res) => {
     try {
         const data = await fs.readFile("./dist/data/data.json", "utf8");
         const users = JSON.parse(data);
@@ -20,13 +20,38 @@ app.get("/data", async (_req, res) => {
     }
 });
 app.post("/data", async (req, res) => {
-    const newArtist = req.body;
-    newArtist.id = new Date().getTime();
+    try {
+        const newArtist = req.body;
+        newArtist.id = new Date().getTime();
+        const data = await fs.readFile("./dist/data/data.json", "utf8");
+        const artists = JSON.parse(data);
+        artists.push(newArtist);
+        fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
+        res.json(artists);
+    }
+    catch (error) {
+        console.log("Error reading data file:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+app.put("/data/:id", async (req, res) => {
+    const id = Number(req.params.id);
     const data = await fs.readFile("./dist/data/data.json", "utf8");
-    const artists = JSON.parse(data);
-    artists.push(newArtist);
+    const artists = await JSON.parse(data);
+    const artistToUpdate = artists.find(artist => artist.id === id);
+    const body = req.body;
+    if (artistToUpdate) {
+        artistToUpdate.name = body.name;
+        artistToUpdate.birthdate = body.birthdate;
+        artistToUpdate.activeSince = body.activeSince;
+        artistToUpdate.genres = body.genres;
+        artistToUpdate.labels = body.labels;
+        artistToUpdate.website = body.website;
+        artistToUpdate.image = body.image;
+        artistToUpdate.shortDescription = body.shortDescription;
+    }
     fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
-    res.json(artists);
+    res.send(artists);
 });
 app.listen(port, () => {
     console.log(`Server is running on ${port}!`);
--- src/backend/server.ts
+++ src/backend/server.ts
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(cors());
 
 //
-app.get("/", (_req, res) => {
+app.get("/", (req, res) => {
     res.send("Root route! 🔥");
 });
 
 //----- GET data ----- //
-app.get("/data", async (_req, res) => {
+app.get("/data", async (req, res) => {
     try {
         const data: string = await fs.readFile("./dist/data/data.json", "utf8");
 
@@ -31,17 +31,55 @@ app.get("/data", async (_req, res) => {
 
 //----- POST data ----- //
 app.post("/data", async (req, res) => {
-    const newArtist = req.body;
-    newArtist.id = new Date().getTime();
+    try {
+        const newArtist: Artist = req.body;
+
+        newArtist.id = new Date().getTime();
+
+        const data: string = await fs.readFile("./dist/data/data.json", "utf8");
+
+        const artists: Artist[] = JSON.parse(data);
+        artists.push(newArtist);
+
+        fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
+
+        res.json(artists);
+    } catch (error) {
+        console.log("Error reading data file:", error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//----- PUT data ----- //
+app.put("/data/:id", async (req, res) => {
+    const id: number = Number(req.params.id);
 
     const data: string = await fs.readFile("./dist/data/data.json", "utf8");
 
-    const artists: object[] = JSON.parse(data);
-    artists.push(newArtist);
+    const artists: Artist[] = await JSON.parse(data);
+
+    const artistToUpdate = artists.find(artist => artist.id === id);
+
+    const body: Artist = req.body;
+
+    if (artistToUpdate) {
+        artistToUpdate.name = body.name;
+        artistToUpdate.birthdate = body.birthdate;
+        artistToUpdate.activeSince = body.activeSince;
+        artistToUpdate.genres = body.genres;
+        artistToUpdate.labels = body.labels;
+        artistToUpdate.website = body.website;
+        artistToUpdate.image = body.image;
+        artistToUpdate.shortDescription = body.shortDescription;
+    }
 
     fs.writeFile("./dist/data/data.json", JSON.stringify(artists));
-    res.json(artists);
+
+    res.send(artists);
 });
+
+//----- DELETE data ----- //
+
 
 //----- Start server on chosen port ----- //
 app.listen(port, () => {
